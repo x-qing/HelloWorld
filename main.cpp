83f8d2b0e5ab9af1864355ba7ec312c505c6c13e
cc <2096238228@qq.com>
2024-05-08 16:32:33 +0800

    添加main.cpp文件，写了初始的代码

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+void sort(int arr[],int size){}
+using namespace std;
+int main(){
+	int arr[] = {12,4,89,43,21,78};
+	int size =  sizeof(arr)/sizeof(srr[0]);
+	sort(arr,size);
+	return 0;
+}
