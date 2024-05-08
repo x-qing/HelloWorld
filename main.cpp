b3fba15b876dac90449ca5bda898608270bce8fe
Qing <2096238228@qq.com>
2024-05-08 19:04:24 +0800

    ubuntu

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@ void sort(int arr[],int size){
 	int temp = 0;
 	for(int i = 0;i<size-1;i++){
 		for(int j = 0;j<size-1-i;j++){
-			if(arr[j] > arr[j+1]){
+			// ubuntu 修改算法，从大到小
+			if(arr[j] < arr[j+1]){
 				temp = arr[j];
 				arr[j]=arr[j+1];
 				arr[j+1]=temp;
