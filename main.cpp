5b8771ccb27b6a0d06df8c38f2604ae93fc42e89
cc <2096238228@qq.com>
2024-05-08 19:45:07 +0800

    修改算法为快排

--- main.cpp
+++ main.cpp
@@ -1,28 +1,42 @@
 #include <iostream>
-// ubuntu add other
-void sort(int arr[],int size){
-	int temp = 0;
-	bool flag = false;
-	for(int i = 0;i<size-1;i++){
-		for(int j = 0;j<size-1-i;j++){
-			// ubuntu 修改算法，从大到小
-			if(arr[j] < arr[j+1]){
-				temp = arr[j];
-				arr[j]=arr[j+1];
-				arr[j+1]=temp;
-				flag = true;
-			}
-			if(!flag){
-				break;
-			}
+using namespace std;
+void sort(int arr[],int i,int j){
+	int val = arr[i];
+	int l = i;
+	int r = j;
+	while(l<r){
+		while(l < r && arr[r]>=val){
+			r--;
+		}
+		if(l < r){
+			arr[l++] = arr[r];
+		}
+		
+		while(l < r && arr[l]<=val){
+			l++;
+		}
+		if(l<r){
+			arr[r--] = arr[l];
 		}
 	}
+	arr[l] = val;
+	
+	sort(arr,i,l-1);
+	sort(arr,l+1,j);
+}
+// ubuntu add other
+void sort(int arr[],int size){
+	sort(arr,0,size);
 }
 using namespace std;
 int main(){
 	// windows添加了注释
 	int arr[] = {12,4,89,43,21,78};
 	int size =  sizeof(arr)/sizeof(arr[0]);
-	sort(arr,size);
+	sort(arr,size); 
+	for(int i = 0;i<size;i++){
+		cout << arr[i] << "  ";
+	}
+	cout << endl;
 	return 0;
 }
