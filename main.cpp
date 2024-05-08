9a0fd954084eea405d8c25c5c17b1eee5b068207
Qing <2096238228@qq.com>
2024-05-08 16:55:31 +0800

    实现sort函数

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-void sort(int arr[],int size){}
+void sort(int arr[],int size){
+	int temp = 0;
+	for(int i = 0;i<size-1;i++){
+		for(int j = 0;j<size-1-i;j++){
+			if(arr[j] > arr[j+1]){
+				temp = arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+}
 using namespace std;
 int main(){
 	int arr[] = {12,4,89,43,21,78};
-	int size =  sizeof(arr)/sizeof(srr[0]);
+	int size =  sizeof(arr)/sizeof(arr[0]);
 	sort(arr,size);
 	return 0;
 }
