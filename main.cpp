ebb3834485d7ce1c268b205d35bc5354f9c35d12
cc <2096238228@qq.com>
2024-05-08 19:07:58 +0800

    win优化排序算法

--- main.cpp
+++ main.cpp
@@ -2,12 +2,17 @@
 // ubuntu add other
 void sort(int arr[],int size){
 	int temp = 0;
+	bool flag = false;
 	for(int i = 0;i<size-1;i++){
 		for(int j = 0;j<size-1-i;j++){
 			if(arr[j] > arr[j+1]){
 				temp = arr[j];
 				arr[j]=arr[j+1];
 				arr[j+1]=temp;
+				flag = true;
+			}
+			if(!flag){
+				break;
 			}
 		}
 	}
