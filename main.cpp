5e2a961b5460006f5f1b7555505d6239b6e22c08
cc <2096238228@qq.com>
2024-05-08 16:58:54 +0800

    修改size计算错误

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ void sort(int arr[],int size){}
 using namespace std;
 int main(){
 	int arr[] = {12,4,89,43,21,78};
-	int size =  sizeof(arr)/sizeof(srr[0]);
+	int size =  sizeof(arr)/sizeof(arr[0]);
 	sort(arr,size);
 	return 0;
 }
